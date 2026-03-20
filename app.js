a5a81d98ba1423f41ae613f841d5867de2e566b7
Adrián López <28adrianls@gmail.com>
2026-03-19 20:10:54 -0600

    Avances FetchAPI

---
+++ app.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const statusText = document.getElementById("status");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", async () => {
+    const query = input.value.trim();
+
+    if (!query) return;
+
+    statusText.textContent = "Cargando...";
+    result.innerHTML = "";
+
+    try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${query}`);
+
+    if (!response.ok) {
+        throw new Error("No encontrado");
+    }
+
+    const data = await response.json();
+    const country = data[0];
+
+    result.innerHTML = `
+        <p><strong>Nombre:</strong> ${country.name.common}</p>
+        <img src="${country.flags.png}" width="150">
+    `;
+
+    statusText.textContent = "";
+
+    } catch (error) {
+        statusText.textContent = "Error: país no encontrado";
+    }
+});
